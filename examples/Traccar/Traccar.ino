7d0d49f62513666773160f90b79b0f054ad8ba1f
lewisxhe <lewishe@outlook.com>
2026-01-23 09:19:21 +0800

    Fixed CI

--- examples/Traccar/Traccar.ino
+++ examples/Traccar/Traccar.ino
@@ -439,7 +439,7 @@ void setup()
 #if defined(TINY_GSM_MODEM_A7670) || defined(TINY_GSM_MODEM_A7608)
     modem.setGPSMode(GNSS_MODE_GPS_BDS_GALILEO_SBAS_QZSS);
 #elif defined(TINY_GSM_MODEM_SIM7670G)
-    modem.setGPSMode(GNSS_MODE_GPS_GLONASS_BDS);
+    modem.setGPSMode(GNSS_MODE_GPS_GLONASS_GALILEO_BDS);
 #elif defined(TINY_GSM_MODEM_SIM7600)
     modem.setGPSMode(GNSS_MODE_ALL);
 #elif defined(TINY_GSM_MODEM_SIM7000SSL) || defined(TINY_GSM_MODEM_SIM7000)
